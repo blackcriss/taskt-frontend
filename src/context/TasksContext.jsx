b857f3b972ddde1b59d4609c8973b488677aec57
Cristian Palacios <cristianypv@hotmail.com>
2025-03-03 23:03:52 -0500

    Update task

--- src/context/TasksContext.jsx
+++ src/context/TasksContext.jsx
@@ -3,6 +3,8 @@ import {
   createTaskRequest,
   getTasksRequest,
   deleteTaskRequest,
+  getTaskRequest,
+  updateTaskRequest,
 } from "../api/tasks";
 
 const TaskContext = createContext();
@@ -43,6 +45,24 @@ export function TaskProvider({ children }) {
     }
   };
 
+  const getTask = async (id) => {
+    try {
+      const res = await getTaskRequest(id);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+
+  const updateTask = async (id, task) => {
+    try {
+      await updateTaskRequest(id, task);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <TaskContext.Provider
       value={{
@@ -50,6 +70,8 @@ export function TaskProvider({ children }) {
         createTask,
         getTasks,
         deleteTask,
+        getTask,
+        updateTask,
       }}
     >
       {children}
