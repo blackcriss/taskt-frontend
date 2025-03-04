4dcdaf3a2ff1323b150244ed2d55f1cb22402a10
Cristian Palacios <cristianypv@hotmail.com>
2025-03-04 00:13:03 -0500

    Retoques

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
 
   return (
     <nav className="bg-zinc-700 my-3 flex justify-between py-5 px-10 rounded-lg">
-      <Link to="/">
+      <Link to={isAuthenticated ? "/tasks" : "/"}>
         <h1 className="text-2xl font-bold">Tasks Manager</h1>
       </Link>
       <ul className="flex gap-x-2">
@@ -14,8 +14,12 @@ function NavBar() {
           <>
             <li>Welcome {user.username}</li>
             <li>
-              <Link to="/add-task"
-              className="bg-indigo-500 px-4 py-1 rounded-sm">Add Task</Link>
+              <Link
+                to="/add-task"
+                className="bg-indigo-500 px-4 py-1 rounded-sm"
+              >
+                Add Task
+              </Link>
             </li>
             <li>
               <Link
@@ -31,12 +35,17 @@ function NavBar() {
         ) : (
           <>
             <li>
-              <Link to="/login"
-              className="bg-indigo-500 px-4 py-1 rounded-sm">Login</Link>
+              <Link to="/login" className="bg-indigo-500 px-4 py-1 rounded-sm">
+                Login
+              </Link>
             </li>
             <li>
-              <Link to="/register"
-              className="bg-indigo-500 px-4 py-1 rounded-sm">Register</Link>
+              <Link
+                to="/register"
+                className="bg-indigo-500 px-4 py-1 rounded-sm"
+              >
+                Register
+              </Link>
             </li>
           </>
         )}
